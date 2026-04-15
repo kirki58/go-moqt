3ef9dca415234e38dd4fa7d7ab4fbc9a5e274efe
kirki58 <burakkirikk@gmail.com>
2026-04-15 15:23:41 +0300

    simplified the mock track encoder, figured that there is no need for prngs in payload generation, i now only embed the location of the object in the payload

--- h264/mock_encoder.go
+++ h264/mock_encoder.go
@@ -1,24 +1,20 @@
 package h264
 
 import (
-	"encoding/binary"
+	"go-moq/pkg/message"
 	"go-moq/pkg/model"
 	"go-moq/pkg/session"
-	"math/rand/v2"
 )
 
 // This is a mock for an encoder, it will act like it is actively encoding and "dispatching" network objects to the publisher via the dispatcher.
 // Though there is no real encoding, it will dispatch a fake track to the publisher.
 // A 1:1 mapping exists between an encoder and a dispatcher
 
-// const TRACK_SIZE = 1800 // 60 objects per 30 groups
-const PAYLOAD_SIZE = 8 // uint64s, x 8 = 64 bytes
-
 type MockEncoder struct {
 	dispatcher *session.Dispatcher
 }
 
-func NewMockEncoder(disp *session.Dispatcher) *MockEncoder{
+func NewMockEncoder(disp *session.Dispatcher) *MockEncoder {
 	return &MockEncoder{
 		dispatcher: disp,
 	}
@@ -27,7 +23,7 @@ func NewMockEncoder(disp *session.Dispatcher) *MockEncoder{
 // will create and dispatch track_size amount of objects, will increment object group id for each group_size objects
 // object payloads are psuedo-randomly generated with the given 2 seeds.
 func (en *MockEncoder) Encode(seed1, seed2 uint64, trackSize, groupSize uint16, ftn *model.MoqtFullTrackName) {
-	if groupSize > trackSize{
+	if groupSize > trackSize {
 		return
 	}
 
@@ -35,32 +31,26 @@ func (en *MockEncoder) Encode(seed1, seed2 uint64, trackSize, groupSize uint16, 
 	latestGroup := uint16(0)
 	latestObj := uint16(0)
 
-	// Psuedo-random payload generation
-	pcg := rand.NewPCG(seed1, seed2) // psuedo-random number generator to generate payloads for us
-
 	for i := uint16(0); i < trackSize; i++ {
 		// in-stack slice to prevent unnecessary heap allocation to store randomly-generated payload
-		var payloadBytesArr [PAYLOAD_SIZE * 8]byte
-		payloadBytes := payloadBytesArr[:]
+		var payloadBytesArr [128]byte
+		payloadBytes := payloadBytesArr[:0]
 
-		// Put a single uint64 as 8 bytes into the slice in little-endian fashion (to provide conventionality across test platforms)
-		for i := 0; i < PAYLOAD_SIZE; i++ {
-			n := pcg.Uint64()
-			binary.LittleEndian.PutUint64(payloadBytes[i*8:], n)
-		}
+		loc := model.MoqtLocation{GroupId: uint64(latestGroup), ObjectId: uint64(latestObj)}
+		message.EncodeMoqtLocation(&payloadBytes, loc)
 
 		obj := &model.MoqtObject{
-			Location: model.MoqtLocation{GroupId: uint64(latestGroup), ObjectId: uint64(latestObj)},
-			SubgroupID: 0, // Not important right now
-			FullTrackName: *ftn,
-			PublisherPriority: 128, // Not important
+			Location:                   loc,
+			SubgroupID:                 0, // Not important right now
+			FullTrackName:              *ftn,
+			PublisherPriority:          128, // Not important
 			ObjectForwardingPreference: model.Subgroup,
-			ObjectStatus: model.Normal,
-			Payload: payloadBytes,
+			ObjectStatus:               model.Normal,
+			Payload:                    payloadBytes,
 		}
 		en.dispatcher.Dispatch(obj)
 
-		if latestObj == groupSize - 1 {
+		if latestObj == groupSize-1 {
 			// End of group reached
 			latestGroup++
 		}
